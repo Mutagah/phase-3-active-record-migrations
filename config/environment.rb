a40ca6000a7ffa00edf45d7355a6017aec4024c1
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-08-29 16:06:44 +0300

    commit tree 000a6676eeacc71d612c461aee7504b33e125d58

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,11 @@
+# ENV["RACK_ENV"] is an environment variable.
+#Environment variables determine which environment our code is running.
+#The development database is used which is configured in our database.yml
+# require_relative "./database.yml"
 ENV["RACK_ENV"] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
 require_relative "../app/models/artist"
+
